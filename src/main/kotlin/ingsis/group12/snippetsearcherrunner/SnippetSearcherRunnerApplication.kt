aac6da57f1914bcf465b23e4cfaeb8d136b8e4ed
EmiLopez23 <emiliano.lopez@ing.austral.edu.ar>
2024-04-30 16:43:44 -0300

    first commit

---
+++ src/main/kotlin/ingsis/group12/snippetsearcherrunner/SnippetSearcherRunnerApplication.kt
@@ -0,0 +1,11 @@
+package ingsis.group12.snippetsearcherrunner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetSearcherRunnerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SnippetSearcherRunnerApplication>(*args)
+}
---
+++ src/test/kotlin/ingsis/group12/snippetsearcherrunner/SnippetSearcherRunnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package ingsis.group12.snippetsearcherrunner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SnippetSearcherRunnerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
